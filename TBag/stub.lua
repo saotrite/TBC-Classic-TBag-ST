6be59287839355bc6089dda59f277cda2a37a84d
saotrite <sao.triton@pm.me>
2026-01-30 17:04:56 -0600

    Pushing Compatability changes from https://www.curseforge.com/wow/addons/tbag-gagstv

---
+++ TBag/stub.lua
@@ -0,0 +1,7 @@
+TBag = CreateFrame("Frame","TBag",UIParent)
+TBag:RegisterEvent("VARIABLES_LOADED")
+
+TBag.LibDD = LibStub:GetLibrary("TBag-LibUIDropDownMenu-4.0")
+
+TBag.BankHaveNil = 0
+TBag.BagHaveNil = 0
