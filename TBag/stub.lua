ef20efb37fa4b9c42f090bc8d1d3b38d84496ac1
saotrite <sao.triton@pm.me>
2026-01-30 16:58:54 -0600

    Revert "Bringing over TBag and Compatability changes"
    This reverts commit 5c7521de4e39d8a8db8a637e0f01c45abe98cf79.

--- TBag/stub.lua
+++
@@ -1,7 +0,0 @@
-TBag = CreateFrame("Frame","TBag",UIParent)
-TBag:RegisterEvent("VARIABLES_LOADED")
-
-TBag.LibDD = LibStub:GetLibrary("TBag-LibUIDropDownMenu-4.0")
-
-TBag.BankHaveNil = 0
-TBag.BagHaveNil = 0
